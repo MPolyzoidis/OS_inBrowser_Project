353c71c197a32361df238406a1e6823de33a5228
MPolyzoidis <polyzoidism@gmail.com>
2024-09-02 16:23:15 +0300

    Taskbar.css update
    Fixed some CSS

--- react-portofolio/src/Components/Taskbar.jsx
+++ react-portofolio/src/Components/Taskbar.jsx
@@ -5,18 +5,22 @@ import startBtnIcon from "../assets/windows98Icons/Win98Icon.png"
 
 const Taskbar = () => {
     return (
-      <div className="taskbar">
-        <div className="taskbar-start-button">
-          <img src={startBtnIcon} alt="Start" />
-          <span>Start</span>
+        <div className="taskbar">
+
+            <div className="taskbar-start-button">
+                <img src={startBtnIcon} alt="Start" />
+                <span>Start</span>
+            </div>
+
+            <div className="task-area">
+                {/* Task area for opened applications */}
+            </div>
+
+            <div className="system-tray">
+                <div className="taskbar-name">Marios Polyzoidis</div>
+            </div>
+            
         </div>
-        <div className="task-area">
-          {/* Task area for opened applications */}
-        </div>
-        <div className="system-tray">
-          <div className="taskbar-name">Marios Polyzoidis</div>
-        </div>
-      </div>
     );
   };
   
